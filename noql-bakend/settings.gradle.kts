24472eb3b53af11a6d697d4ce2e5d805fa36301f
Jan Babák <babakhonza@gmail.com>
2023-11-19 15:33:22 +0100

    Add: noql-backend

---
+++ noql-bakend/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "noql-bakend"
---
+++ noql-bakend/src/test/java/com/janbabak/noqlbakend/NoqlBakendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janbabak.noqlbakend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoqlBakendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
