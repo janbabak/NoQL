4c07c7735f8b4eea3a01cb12d7ac587360470b8c
Jan Babák <babakhonza@gmail.com>
2023-11-26 19:08:43 +0100

    Fix typo in the project name

---
+++ noql-backend/src/test/java/com/janbabak/noqlbackend/NoQLBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janbabak.noqlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoqlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
