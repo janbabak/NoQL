7504aefd4696cd1ade08a95259ac186e60dd9457
Jan Babák <61386198+janbabak@users.noreply.github.com>
2023-11-26 18:27:54 +0100

    Finish GPT API (#13)
    * Add: GPT API
    
    * Add: Use spring requestTemplate, add data types, remove payload

---
+++ noql-bakend/src/main/java/com/janbabak/noqlbakend/model/gpt/GptResponse.java
@@ -0,0 +1,36 @@
+package com.janbabak.noqlbakend.model.gpt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GptResponse {
+    private String id;
+    private String object;
+    private String model;
+    private Usage usage;
+    private List<Choice> choices;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Usage {
+        private int prompt_tokens;
+        private int completion_tokens;
+        private int total_tokens;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Choice {
+        private GptQuery.Message message;
+        private String finish_reason;
+        private int index;
+    }
+}
