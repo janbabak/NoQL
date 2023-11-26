682862d3ba0bc0c04184667b490354a1c00f1954
Jan Babák <61386198+janbabak@users.noreply.github.com>
2023-11-26 17:45:39 +0100

    Revert "Add: GPT API (#10)" (#11)
    This reverts commit b11c284c323680ffa83399d47e7f2f8f9d1e1c25.

--- noql-bakend/src/main/java/com/janbabak/noqlbakend/NoqlBakendApplication.java
+++ noql-bakend/src/main/java/com/janbabak/noqlbakend/NoqlBakendApplication.java
@@ -1,7 +1,5 @@
 package com.janbabak.noqlbakend;
 
-import com.janbabak.noqlbakend.service.api.GptApi;
-import com.janbabak.noqlbakend.service.api.QueryApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -19,11 +17,5 @@ public class NoqlBakendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoqlBakendApplication.class, args);
-		try {
-			QueryApi gptApi = new GptApi();
-			System.out.println(gptApi.queryModel("What is the most common name in the USA."));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
 	}
 }
