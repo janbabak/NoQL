b11c284c323680ffa83399d47e7f2f8f9d1e1c25
Jan Babák <61386198+janbabak@users.noreply.github.com>
2023-11-26 17:42:25 +0100

    Add: GPT API (#10)

--- noql-bakend/src/main/java/com/janbabak/noqlbakend/NoqlBakendApplication.java
+++ noql-bakend/src/main/java/com/janbabak/noqlbakend/NoqlBakendApplication.java
@@ -1,5 +1,7 @@
 package com.janbabak.noqlbakend;
 
+import com.janbabak.noqlbakend.service.api.GptApi;
+import com.janbabak.noqlbakend.service.api.QueryApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -17,5 +19,11 @@ public class NoqlBakendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoqlBakendApplication.class, args);
+		try {
+			QueryApi gptApi = new GptApi();
+			System.out.println(gptApi.queryModel("What is the most common name in the USA."));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
