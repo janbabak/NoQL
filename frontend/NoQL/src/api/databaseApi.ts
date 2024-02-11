2a5d6a12861e74889a278a4cca4d05266d3ab604
Jan Babák <babakhonza@gmail.com>
2024-02-11 11:02:53 +0100

    create query result view

--- frontend/NoQL/src/api/databaseApi.ts
+++ frontend/NoQL/src/api/databaseApi.ts
@@ -1,16 +1,27 @@
 import Api from '@/api/api'
 import type { AxiosResponse } from 'axios'
+import type { RouteParamValue } from 'vue-router'
 
 interface Database {
-  id: string,
-  name: string,
-  host: string,
-  port: number,
-  database: string,
-  userName: string,
-  password: string,
-  engine: string, // TODO: create enum
+  id: string
+  name: string
+  host: string
+  port: number
+  database: string
+  userName: string
+  password: string
+  engine: string // TODO: create enum
   isSQL: boolean
+}
+
+interface QueryResult {
+  columnNames: string[]
+  rows: string[][]
+}
+
+interface QueryResponse {
+  query: string
+  result: QueryResult
 }
 
 const databaseApi = {
@@ -19,8 +30,16 @@ const databaseApi = {
 
   getAll(): Promise<AxiosResponse<Database[]>> {
     return this.API.get(this.DOMAIN)
+  },
+
+  getById(id: string | RouteParamValue[]): Promise<AxiosResponse<Database>> {
+    return this.API.get(this.DOMAIN + '/' + id)
+  },
+
+  queryNaturalLanguage(id: string | RouteParamValue[], query: string): Promise<AxiosResponse<QueryResponse>> {
+    return this.API.post(this.DOMAIN + '/' + id + '/query/natural-language', query)
   }
 }
 
 export default databaseApi
-export type { Database }
+export type { Database, QueryResponse }
