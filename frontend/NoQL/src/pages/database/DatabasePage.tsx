9804ec28cfb95d71443bdf0882a809bde1836212
Jan Babák <61386198+janbabak@users.noreply.github.com>
2024-03-24 10:07:05 +0100

    Add editor for query language query (#94)

--- frontend/NoQL/src/pages/database/DatabasePage.tsx
+++ frontend/NoQL/src/pages/database/DatabasePage.tsx
@@ -2,11 +2,12 @@ import { useParams } from 'react-router'
 import { useEffect, useRef, useState } from 'react'
 import { Database } from '../../types/Database.ts'
 import databaseApi, { QueryResponse } from '../../services/api/databaseApi.ts'
-import { Alert, TextField, Typography } from '@mui/material'
+import { Alert, Box, Button, Tab, Tabs, TextField, Typography } from '@mui/material'
 import { LoadingButton } from '@mui/lab'
 import styles from './Database.module.css'
 import { ResultTable } from './ResultTable.tsx'
 import { GeneratedQuery } from './GeneratedQuery.tsx'
+import { Editor } from '@monaco-editor/react'
 
 export function DatabasePage() {
   const { id } = useParams<string>()
@@ -32,22 +33,12 @@ export function DatabasePage() {
   ] = useState<boolean>(false)
 
   const [
-    page,
-    setPage
-  ] = useState<number>(0)
-
-  const [
-    pageSize,
-    setPageSize
-  ] = useState<number>(10)
-
-  const [
-    totalCount,
-    setTotalCount
-  ] = useState<number | null>(0)
+    showGeneratedQuery,
+    setShowGeneratedQuery
+  ] = useState<boolean>(true)
 
   /* eslint-disable  @typescript-eslint/no-explicit-any */
-  const usersQuery = useRef<any>('')
+  const naturalLanguageQuery = useRef<any>(null)
 
   useEffect(() => {
     void loadDatabase()
@@ -72,16 +63,38 @@ export function DatabasePage() {
     setPage(0)
     setQueryLoading(true)
     try {
-      const response =
-        await databaseApi.queryNaturalLanguage(id || '', usersQuery.current.value, pageSize)
+      const naturalLanguage = tab == 0
+      const response = naturalLanguage
+        ? await databaseApi.queryNaturalLanguage(
+          id || '', naturalLanguageQuery.current.value, pageSize)
+        : await databaseApi.queryQueryLanguageQuery(
+          id || '', editor.current.getValue(), 0, pageSize)
       setQueryResult(response.data)
       setTotalCount(response.data.totalCount)
     } catch (error: unknown) {
       console.log(error) // TODO: handles
     } finally {
       setQueryLoading(false)
+      setShowGeneratedQuery(true)
     }
   }
+
+  // pagination-------------------------------------------------------------------------------------
+
+  const [
+    page,
+    setPage
+  ] = useState<number>(0)
+
+  const [
+    pageSize,
+    setPageSize
+  ] = useState<number>(10)
+
+  const [
+    totalCount,
+    setTotalCount
+  ] = useState<number | null>(0)
 
   // get next page
   async function onPageChange(page: number, pageSize: number) {
@@ -111,15 +124,60 @@ export function DatabasePage() {
     onPageChange(0, newPageSize)
   }
 
+  // tabs-------------------------------------------------------------------------------------------
+
+  const [
+    tab,
+    setTab
+  ] = useState<number>(0)
+
+  function handleTabChange(_event: React.SyntheticEvent, newValue: number) {
+    setTab(newValue)
+  }
+
+  // editor-----------------------------------------------------------------------------------------
+
+  const editor = useRef<any>(null)
+
+  const monaco = useRef<any>(null)
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  function handleEditorDidMount(editorParam: string, monacoParam: unknown): void {
+    editor.current = editorParam
+    monaco.current = monacoParam
+  }
+
+  function editQueryInEditor(query: string) {
+    monaco.current?.editor.getModels()[0].setValue(query)
+    setTab(1)
+    setShowGeneratedQuery(false)
+  }
+
+  const EditQueryButttom =
+    <Button
+      onClick={() => editQueryInEditor(queryResult?.query || '')}
+      size="small"
+      color="inherit"
+    >
+      Edit query
+    </ Button>
+
+  // JSX--------------------------------------------------------------------------------------------
+
   const QueryResultElement =
     <>
       {queryResult != null &&
         <div>
-          <GeneratedQuery query={queryResult.query} />
+
+          {showGeneratedQuery &&
+            <GeneratedQuery query={queryResult.query} />
+          }
 
 
           {queryResult.errorMessage != null &&
-            <Alert severity="error">{queryResult.errorMessage}</Alert>
+            <Alert severity="error" action={EditQueryButttom}>
+              {queryResult.errorMessage}
+            </Alert>
           }
 
           {totalCount != null &&
@@ -140,13 +198,45 @@ export function DatabasePage() {
     <>
       <Typography variant="h4" component="h2">{database?.name}</Typography>
 
-      <div className={styles.queryInput}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          value={tab}
+          aria-label="basic tabs example"
+          onChange={handleTabChange}
+          sx={{ borderRadius: '0.25rem', marginTop: '1.5rem' }}
+        >
+          <Tab label="Natural language" sx={{ borderRadius: '0.25rem' }} />
+          <Tab label="Query language" sx={{ borderRadius: '0.25rem' }} />
+        </Tabs>
+      </Box>
+
+      <div className={styles.queryInput} role="tabpanel" hidden={tab != 0}>
         <TextField
           id="query"
           label="Query"
           variant="outlined"
-          inputRef={usersQuery}
+          inputRef={naturalLanguageQuery}
           fullWidth
+        />
+      </div>
+
+      <div role="tabpanel" hidden={tab != 1} className={styles.queryEditor}>
+        {/*https://microsoft.github.io/monaco-editor/docs.html#interfaces/editor.IStandaloneEditorConstructionOptions.html*/}
+        <Editor
+          height="200px"
+          language="sql"
+          theme="vs-dark"
+          onMount={handleEditorDidMount}
+          options={{
+            inlineSuggest: true,
+            fontSize: 16,
+            fontFamily: 'monospace',
+            lineHeight: 24,
+            formatOnType: true,
+            autoClosingBrackets: true,
+            minimap: { enabled: false },
+            padding: { top: 20 }
+          }}
         />
       </div>
 
