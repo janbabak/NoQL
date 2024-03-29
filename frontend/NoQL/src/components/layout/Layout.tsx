9e90556b5e5f6cf4bfbd25aef447d92d78411645
Jan Babák <babakhonza@gmail.com>
2024-03-29 09:59:52 +0100

    create PersistentDrawer component

--- frontend/NoQL/src/components/layout/Layout.tsx
+++ frontend/NoQL/src/components/layout/Layout.tsx
@@ -1,111 +1,29 @@
 import { Outlet } from 'react-router-dom'
-// import styles from './Layout.module.css'
-// import { NavBar } from './NavBar.tsx'
-//
-// export function Layout() {
-//   return (
-//     <>
-//       {/*<header className={styles.header}>header</header>*/}
-//       <NavBar />
-//       <main className={styles.main}>
-//         <Outlet />
-//       </main>
-//       <footer className={styles.footer}>footer</footer>
-//     </>
-//   )
-// }
-
 import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { CustomAppBar } from './CusomAppBar.tsx'
 import { Main } from './Main.tsx'
-
-const drawerWidth = 240;
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
-}));
+import { PersistentDrawer } from './PersistentDrawer.tsx'
 
 export function Layout() {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const [
+    open,
+    setOpen
+  ] = React.useState(false);
 
   const handleDrawerOpen = () => {
     setOpen(true);
   };
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
   return (
     <Box sx={{ display: 'flex' }}>
+
       <CssBaseline />
+
       <CustomAppBar handleDrawerOpen={handleDrawerOpen} open={open} />
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+
+      <PersistentDrawer open={open} setOpen={setOpen}/>
 
       <Main open={open}>
         <Outlet />
