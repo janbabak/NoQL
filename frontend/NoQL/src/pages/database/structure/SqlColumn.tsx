a22030a0ebbe42284f1a3a4c58e11ec9906988a5
Jan Babák <babakhonza@gmail.com>
2024-04-01 12:00:48 +0200

    Add hyperlink that opens reference in db structure

--- frontend/NoQL/src/pages/database/structure/SqlColumn.tsx
+++ frontend/NoQL/src/pages/database/structure/SqlColumn.tsx
@@ -1,23 +1,47 @@
-import { Column } from '../../../types/DatabaseStructure.ts'
+import { Column, ForeignKey } from '../../../types/DatabaseStructure.ts'
 import KeyRoundedIcon from '@mui/icons-material/KeyRounded'
 import styles from './Structure.module.css'
 
 interface SqlColumnProps {
-  column: Column
+  column: Column,
+  openForeignKey: (foreignKey: ForeignKey) => void,
+  id: string,
 }
 
-export function SqlColumn({ column }: SqlColumnProps) {
+export function SqlColumn({ column, openForeignKey, id }: SqlColumnProps) {
+
+  // format foreign column name to format schema.table.columnName
+  function getFormattedColumnName(): string | null {
+    if (!column.foreignKey) {
+      return null
+    }
+    let name: string = column.foreignKey.referencedSchema + '.'
+
+    // remove quotes from the table name if its surround by them
+    if (column.foreignKey.referencedTable[0] == '"'
+      && column.foreignKey.referencedTable[column.foreignKey.referencedTable.length - 1] == '"') {
+      name += column.foreignKey.referencedTable.substring(1, column.foreignKey.referencedTable.length - 1)
+    } else {
+      name = column.foreignKey.referencedTable
+    }
+
+    return name + '.' + column.foreignKey.referencedColumn
+  }
+
   const PrimaryKey = <KeyRoundedIcon className={styles.primaryKeyIcon} />
+
   const ForeignKey =
     <>
       {column.foreignKey != null &&
         <>
           <KeyRoundedIcon className={styles.foreignKeyIcon} />
-          <span className={styles.foreignKeyLabel}>
-            {column.foreignKey.referencedSchema + '.'
-              + column.foreignKey.referencedTable + '.'
-              + column.foreignKey.referencedColumn}
-          </span>
+          <a
+            href={'#' + getFormattedColumnName()}
+            onClick={() => openForeignKey(column.foreignKey)}
+            className={styles.foreignKeyLabel}
+          >
+            {getFormattedColumnName()}
+          </a>
         </>
       }
       <span className={styles.columnDataType}>
@@ -26,7 +50,7 @@ export function SqlColumn({ column }: SqlColumnProps) {
     </>
 
   return (
-    <div className={styles.column}>
+    <div className={styles.column} id={id}>
       {column.isPrimaryKey && PrimaryKey}
       <span
         style={{ marginLeft: column.isPrimaryKey ? '0' : '3rem' }}
