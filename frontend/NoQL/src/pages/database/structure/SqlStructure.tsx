bfed32eec8574ba58d474a62ed21c29d08045573
Jan Babák <61386198+janbabak@users.noreply.github.com>
2024-04-01 10:09:02 +0200

    Change default structure expansion and add expansion buttons (#125)

--- frontend/NoQL/src/pages/database/structure/SqlStructure.tsx
+++ frontend/NoQL/src/pages/database/structure/SqlStructure.tsx
@@ -3,15 +3,74 @@ import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView'
 import { TreeItem } from '@mui/x-tree-view/TreeItem'
 import { Schema } from '../../../types/DatabaseStructure.ts'
 import { SqlColumn } from './SqlColumn.tsx'
+import { useState } from 'react'
+import { Button } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 
 interface SqlStructureProps {
   structure: SqlDatabaseStructure
 }
 
 export function SqlStructure({ structure }: SqlStructureProps) {
+  const [
+    expandedItems,
+    setExpandedItems
+  ] = useState<string[]>(structure.schemas.map((_: Schema, index: number) => 's' + index))
+
+  function handleExpandedItemsChange(_event: React.SyntheticEvent, itemIds: string[]) {
+    setExpandedItems(itemIds)
+  }
+
+  // expand all items in the tree view
+  function expandAll(): void {
+    const expanded: string[] = []
+
+    structure.schemas.forEach((schema: Schema, schemaIndex: number): void => {
+      expanded.push('s' + schemaIndex)
+
+      schema.tables.forEach((table: Table, tableIndex: number): void => {
+        expanded.push('s' + schemaIndex + 't' + tableIndex)
+
+        table.columns.forEach((_: Column, columnIndex: number): void => {
+          expanded.push('s' + schemaIndex + 't' + tableIndex + 'c' + columnIndex)
+        })
+      })
+    })
+
+    setExpandedItems(expanded)
+  }
+
+  // collapse all items in the tree view
+  function collapseAll(): void {
+    setExpandedItems([])
+  }
+
+  const ExpansionButtons =
+    <div style={{marginBottom: '1rem'}}>
+      <Button
+        startIcon={<ExpandMoreIcon />}
+        onClick={expandAll}
+      >
+        Expand all
+      </Button>
+
+      <Button
+        startIcon={<ExpandLessIcon />}
+        onClick={collapseAll}
+      >
+        Collapse all
+      </Button>
+    </div>
+
   return (
     <>
-      <SimpleTreeView>
+      {ExpansionButtons}
+
+      <SimpleTreeView
+        expandedItems={expandedItems}
+        onExpandedItemsChange={handleExpandedItemsChange}
+      >
         {
           structure.schemas.map((schema: Schema, schemaIndex: number) => {
             return (
