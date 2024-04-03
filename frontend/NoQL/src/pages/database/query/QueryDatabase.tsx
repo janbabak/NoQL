34913c962ef4d8a2a4a34610b44b8715be201f03
Jan Babák <babakhonza@gmail.com>
2024-04-03 19:05:07 +0200

    Delete QueryInputTabs.tsx

--- frontend/NoQL/src/pages/database/query/QueryDatabase.tsx
+++ frontend/NoQL/src/pages/database/query/QueryDatabase.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { NATURAL_LANGUAGE_TAB } from './Constants.ts'
-import { Typography } from '@mui/material'
-import { QueryInputTabs } from './QueryInputTabs.tsx'
+import { Box, Tab, Tabs, Typography } from '@mui/material'
 import { Database } from '../../../types/Database.ts'
+import { ChatTab } from './ChatTab.tsx'
+import { ConsoleTab } from './ConsoleTab.tsx'
 
 interface QueryDatabaseProps {
   databaseId: string,
@@ -27,6 +28,34 @@ export function QueryDatabase({ databaseId, database, databaseLoading }: QueryDa
     setTab(1)
   }
 
+  function handleTabChange(_event: React.SyntheticEvent, newValue: number): void {
+    setTab(newValue)
+  }
+
+  const TabsView =
+    <>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          value={tab}
+          onChange={handleTabChange}
+          sx={{ borderRadius: '0.25rem', marginTop: '1.5rem' }}
+          aria-label="Chat and Console tabs"
+        >
+          <Tab label="Chat" sx={{ borderRadius: '0.25rem' }} />
+          <Tab label="Editor" sx={{ borderRadius: '0.25rem' }} />
+        </Tabs>
+      </Box>
+
+      <ChatTab databaseId={databaseId} tab={tab} />
+
+      <ConsoleTab
+        databaseId={databaseId}
+        tab={tab}
+        queryLanguageQuery={queryLanguageQuery}
+        setQueryLanguageQuery={setQueryLanguageQuery}
+      />
+    </>
+
   return (
     <>
       <Typography variant="h2" component="h1">Query</Typography>
@@ -34,14 +63,7 @@ export function QueryDatabase({ databaseId, database, databaseLoading }: QueryDa
       {!databaseLoading &&
         <>
           <Typography variant="h4" component="h2">{database?.name}</Typography>
-
-          <QueryInputTabs
-            databaseId={databaseId}
-            tab={tab}
-            setTab={setTab}
-            queryLanguageQuery={queryLanguageQuery}
-            setQueryLanguageQuery={setQueryLanguageQuery}
-          />
+          {TabsView}
         </>
       }
     </>
