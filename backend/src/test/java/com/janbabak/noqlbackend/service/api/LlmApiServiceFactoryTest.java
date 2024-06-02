5051d5ac979d369cd0693fbdbc96d9f6f5cb03d3
Jan Babák <61386198+janbabak@users.noreply.github.com>
2024-06-02 19:25:21 +0200

    Test LlmApiServiceFactory (#208)

---
+++ backend/src/test/java/com/janbabak/noqlbackend/service/api/LlmApiServiceFactoryTest.java
@@ -0,0 +1,53 @@
+package com.janbabak.noqlbackend.service.api;
+
+import com.janbabak.noqlbackend.model.query.gpt.LlmModel;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LlmApiServiceFactoryTest {
+
+    @ParameterizedTest
+    @MethodSource("testGetQueryApiServiceDataProvider")
+    void testGetQueryApiService(LlmModel model, Class<?> expected) {
+        // when
+        QueryApi queryApiService = LlmApiServiceFactory.getQueryApiService(model);
+
+        // then
+        assertInstanceOf(expected, queryApiService);
+    }
+
+    static Object[][] testGetQueryApiServiceDataProvider() {
+        return new Object[][]{
+                {
+                        LlmModel.GPT_4o,
+                        GptApiService.class
+                },
+                {
+                        LlmModel.GPT_4,
+                        GptApiService.class
+                },
+                {
+                        LlmModel.GPT_4_TURBO,
+                        GptApiService.class
+                },
+                {
+                        LlmModel.GPT_4_32K,
+                        GptApiService.class
+                },
+                {
+                        LlmModel.GPT_3_5_TURBO,
+                        GptApiService.class
+                },
+                {
+                        LlmModel.LLAMA3_70B,
+                        LlamaApiService.class
+                },
+                {
+                        LlmModel.LLAMA3_13B_CHAT,
+                        LlamaApiService.class
+                }
+        };
+    }
+}
