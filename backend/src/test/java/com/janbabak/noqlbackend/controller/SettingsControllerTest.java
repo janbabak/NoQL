2b3712ae10487c5ea9ba3153b971fceae4a1f281
Jan Babák <babakhonza@gmail.com>
2024-06-02 18:08:55 +0200

    Add settings controller test

---
+++ backend/src/test/java/com/janbabak/noqlbackend/controller/SettingsControllerTest.java
@@ -0,0 +1,45 @@
+package com.janbabak.noqlbackend.controller;
+
+import com.janbabak.noqlbackend.model.Settings;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@WebMvcTest(SettingsController.class)
+class SettingsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Settings settings;
+
+    @Test
+    @DisplayName("Get settings")
+    void getSettings() throws Exception {
+        // when
+        when(settings.getMaxPageSize()).thenReturn(50);
+        when(settings.getDefaultPageSize()).thenReturn(10);
+        when(settings.getTranslationRetries()).thenReturn(3);
+
+        // then
+        mockMvc.perform(get("/settings"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                             "maxPageSize": 50,
+                             "defaultPageSize": 10,
+                             "translationRetries": 3
+                        }"""));
+    }
+}
