389b8bf58905c99c4d36b4a352f00d20ff417f5c
Jan Babák <61386198+janbabak@users.noreply.github.com>
2024-06-02 19:15:25 +0200

    Test LlamaApiService (#206)

---
+++ backend/src/test/java/com/janbabak/noqlbackend/service/api/LlamaApiServiceTest.java
@@ -0,0 +1,45 @@
+package com.janbabak.noqlbackend.service.api;
+
+import com.janbabak.noqlbackend.model.query.QueryRequest;
+import com.janbabak.noqlbackend.model.query.gpt.LlmModel;
+import org.apache.coyote.BadRequestException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LlamaApiServiceTest {
+
+    private final LlamaApiService llamaApiService = new LlamaApiService();
+
+    @ParameterizedTest
+    @MethodSource("validRequestDataProvider")
+    void testValidateSuccessfulRequest(QueryRequest request) {
+        assertDoesNotThrow(() -> llamaApiService.validateRequest(request));
+    }
+
+    static QueryRequest[] validRequestDataProvider() {
+        return new QueryRequest[]{
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", LlmModel.LLAMA3_70B),
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", LlmModel.LLAMA3_13B_CHAT),
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("badRequestDataProvider")
+    void testValidateBadRequest(QueryRequest request) {
+        assertThrows(BadRequestException.class, () -> llamaApiService.validateRequest(request));
+    }
+
+    static QueryRequest[] badRequestDataProvider() {
+        return new QueryRequest[]{
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", LlmModel.GPT_4o),
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", LlmModel.GPT_4),
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", LlmModel.GPT_4_TURBO),
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", LlmModel.GPT_4_32K),
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", LlmModel.GPT_3_5_TURBO)
+        };
+    }
+}
