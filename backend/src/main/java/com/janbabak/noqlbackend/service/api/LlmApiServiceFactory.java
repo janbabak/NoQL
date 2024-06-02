16732fb3b214e39bf43e08ee0093bd8de78a8bb3
Jan Babák <babakhonza@gmail.com>
2024-06-02 19:24:43 +0200

    Test LlmApiServiceFactory

--- backend/src/main/java/com/janbabak/noqlbackend/service/api/LlmApiServiceFactory.java
+++ backend/src/main/java/com/janbabak/noqlbackend/service/api/LlmApiServiceFactory.java
@@ -9,10 +9,12 @@ public class LlmApiServiceFactory {
      * @param model large language model to be used for the translation
      * @return correct query API service
      */
+    @SuppressWarnings("all") // default branch is unnecessary
     public static QueryApi getQueryApiService(LlmModel model) {
         return switch (model) {
             case GPT_4o, GPT_4, GPT_4_TURBO, GPT_4_32K, GPT_3_5_TURBO -> new GptApiService();
             case LLAMA3_70B, LLAMA3_13B_CHAT -> new LlamaApiService();
+            default -> throw new IllegalArgumentException("Unknown model: " + model);
         };
     }
 }
