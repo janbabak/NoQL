db8e2b9cbb4aa4564f523b3728a040c9eb8128e7
Jan Babák <babakhonza@gmail.com>
2024-06-23 11:19:29 +0200

    fix resource try-catch in mysql service

--- backend/src/test/java/com/janbabak/noqlbackend/dao/MySqlTest.java
+++ backend/src/test/java/com/janbabak/noqlbackend/dao/MySqlTest.java
@@ -3,19 +3,12 @@ package com.janbabak.noqlbackend.dao;
 
 import com.janbabak.noqlbackend.error.exception.DatabaseConnectionException;
 import com.janbabak.noqlbackend.error.exception.DatabaseExecutionException;
-import com.janbabak.noqlbackend.model.database.DatabaseEngine;
-import com.janbabak.noqlbackend.model.entity.Database;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -27,10 +20,12 @@ abstract public class MySqlTest extends LocalDatabaseTest {
     static final String CONTAINER_NAME = "mysql:8.3.0";
 
     static {
-        databaseContainer = new MySQLContainer<>(CONTAINER_NAME)
-                .withDatabaseName(DATABASE_NAME)
-                .withUsername(DATABASE_USERNAME)
-                .withPassword(DATABASE_PASSWORD);
+        try (MySQLContainer<?> mySQLContainer = new MySQLContainer<>(CONTAINER_NAME)) {
+            databaseContainer = mySQLContainer
+                    .withDatabaseName(DATABASE_NAME)
+                    .withUsername(DATABASE_USERNAME)
+                    .withPassword(DATABASE_PASSWORD);
+        }
     }
 
     @Override
