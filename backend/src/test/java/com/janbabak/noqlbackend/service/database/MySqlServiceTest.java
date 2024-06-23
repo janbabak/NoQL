05e83449d6e51bd9e694e9894960f38e137a7cd2
Jan Babák <babakhonza@gmail.com>
2024-06-23 18:21:52 +0200

    Introduce AbstractSqlServiceTesta

--- backend/src/test/java/com/janbabak/noqlbackend/service/database/MySqlServiceTest.java
+++ backend/src/test/java/com/janbabak/noqlbackend/service/database/MySqlServiceTest.java
@@ -1,12 +1,9 @@
 package com.janbabak.noqlbackend.service.database;
 
-import com.janbabak.noqlbackend.dao.AbstractLocalDatabaseTest;
-import com.janbabak.noqlbackend.error.exception.DatabaseConnectionException;
-import com.janbabak.noqlbackend.error.exception.DatabaseExecutionException;
 import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure;
+import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure.Table;
 import com.janbabak.noqlbackend.model.entity.Database;
 import com.janbabak.noqlbackend.service.utils.FileUtils;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,30 +15,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
 @SpringBootTest
-class MySqlServiceTest extends AbstractLocalDatabaseTest {
-    @SuppressWarnings("FieldCanBeLocal")
-    private MySqlService mySqlService;
+class MySqlServiceTest extends AbstractSqlServiceTest {
 
-    private SqlDatabaseStructure databaseStructure;
+    MySqlServiceTest() {
+        super();
 
-    private Database getDatabase() {
-        return mySqlDatabase;
+        INTEGER_DATA_TYPE = "int";
+        NUMERIC_DATA_TYPE = "decimal";
+        VARCHAR_DATA_TYPE = "varchar";
+        TEXT_DATA_TYPE = "text";
+        CHAR_DATA_TYPE = "char";
+        BOOLEAN_DATA_TYPE = "tinyint";
+        DATE_DATA_TYPE = "date";
+        TIMESTAMP_DATA_TYPE = "timestamp";
     }
-
-    private final String INTEGER_DATA_TYPE = "int";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String NUMERIC_DATA_TYPE = "decimal";
-    private final String VARCHAR_DATA_TYPE = "varchar";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String TEXT_DATA_TYPE = "text";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String CHAR_DATA_TYPE = "char";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String BOOLEAN_DATA_TYPE = "tinyint";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String DATE_DATA_TYPE = "date";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String TIMESTAMP_DATA_TYPE = "timestamp";
 
     /**
      * Get scripts for initialization of the databases
@@ -52,12 +39,52 @@ class MySqlServiceTest extends AbstractLocalDatabaseTest {
                 FileUtils.getFileContent("./src/test/resources/dbInsertScripts/mySql/allTables.sql"));
     }
 
-    @BeforeAll
-    @Override
-    protected void setUp() throws DatabaseConnectionException, DatabaseExecutionException {
-        super.setUp();
-        mySqlService = new MySqlService(getDatabase());
-        databaseStructure = mySqlService.retrieveSchema();
+    protected Database getDatabase() {
+        return mySqlDatabase;
+    }
+
+    protected SqlDatabaseService getSqlService(Database database) {
+        return new MySqlService(database);
+    }
+
+    protected String getDefaultSchema() {
+        return DATABASE_NAME;
+    }
+
+    protected String getCvutSchema() {
+        return getDefaultSchema();
+    }
+
+    protected Table getUserTable() {
+        return getTable(getDefaultSchema(), "user");
+    }
+
+    protected Table getAddressTable() {
+        return getTable(getDefaultSchema(), "address");
+    }
+
+    protected Table getOrderTable() {
+        return getTable(getDefaultSchema(), "order");
+    }
+
+    protected Table getSpecialisationTable() {
+        return getTable(getDefaultSchema(), "specialisation");
+    }
+
+    protected Table getStudentTable() {
+        return getTable(getDefaultSchema(), "student");
+    }
+
+    protected Table getFitWikiTable() {
+        return getTable(getDefaultSchema(), "fit_wiki");
+    }
+
+    protected Table getCourseTable() {
+        return getTable(getDefaultSchema(), "course");
+    }
+
+    protected Table getExamTable() {
+        return getTable(getDefaultSchema(), "exam");
     }
 
     @Test
@@ -90,203 +117,5 @@ class MySqlServiceTest extends AbstractLocalDatabaseTest {
         verifyTable(schema.getTables().get("order"), "order", List.of("id"),
                 List.of("id", "user_id", "order_date", "total_amount", "payment_method", "shipping_address_id",
                         "is_shipped", "tracking_number", "status", "notes"));
-    }
-
-    @Test
-    @DisplayName("Test user table")
-    void testUserTable() {
-        SqlDatabaseStructure.Table table = databaseStructure.getSchemas().get(DATABASE_NAME).getTables().get("user");
-        assertEquals(
-                new SqlDatabaseStructure.Column("id", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("id"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("name", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("name"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("age", INTEGER_DATA_TYPE, false, null),
-                table.getColumns().get("age"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("sex", CHAR_DATA_TYPE, false, null),
-                table.getColumns().get("sex"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("email", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("email"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("created_at", TIMESTAMP_DATA_TYPE, false, null),
-                table.getColumns().get("created_at"));
-    }
-
-    @Test
-    @DisplayName("Test address table")
-    void testAddressTable() {
-        SqlDatabaseStructure.Table table = databaseStructure.getSchemas().get(DATABASE_NAME).getTables().get("address");
-        assertEquals(
-                new SqlDatabaseStructure.Column("id", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("id"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("user_id", INTEGER_DATA_TYPE, false,
-                        new SqlDatabaseStructure.ForeignKey(DATABASE_NAME, "user", "id")),
-                table.getColumns().get("user_id"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("street", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("street"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("city", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("city"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("state", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("state"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("postal_code", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("postal_code"));
-    }
-
-    @Test
-    @DisplayName("Test order table")
-    void testOrderTable() {
-        SqlDatabaseStructure.Table table = databaseStructure.getSchemas().get(DATABASE_NAME).getTables().get("order");
-        assertEquals(
-                new SqlDatabaseStructure.Column("id", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("id"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("user_id", INTEGER_DATA_TYPE, false,
-                        new SqlDatabaseStructure.ForeignKey(DATABASE_NAME, "user", "id")),
-                table.getColumns().get("user_id"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("order_date", DATE_DATA_TYPE, false, null),
-                table.getColumns().get("order_date"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("total_amount", NUMERIC_DATA_TYPE, false, null),
-                table.getColumns().get("total_amount"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("payment_method", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("payment_method"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("shipping_address_id", INTEGER_DATA_TYPE, false,
-                        new SqlDatabaseStructure.ForeignKey(DATABASE_NAME, "address", "id")),
-                table.getColumns().get("shipping_address_id"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("is_shipped", BOOLEAN_DATA_TYPE, false, null),
-                table.getColumns().get("is_shipped"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("tracking_number", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("tracking_number"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("status", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("status"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("notes", TEXT_DATA_TYPE, false, null),
-                table.getColumns().get("notes"));
-    }
-
-    @Test
-    @DisplayName("Test specialisation table")
-    void testSpecialisationTable() {
-        SqlDatabaseStructure.Table table = databaseStructure.getSchemas().get(DATABASE_NAME).getTables().get("specialisation");
-        assertEquals(
-                new SqlDatabaseStructure.Column("id", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("id"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("name", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("name"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("manager", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("manager"));
-    }
-
-    @Test
-    @DisplayName("Test student table")
-    void testStudentTable() {
-        SqlDatabaseStructure.Table table = databaseStructure.getSchemas().get(DATABASE_NAME).getTables().get("student");
-        assertEquals(
-                new SqlDatabaseStructure.Column("id", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("id"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("name", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("name"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("birthdate", DATE_DATA_TYPE, false, null),
-                table.getColumns().get("birthdate"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("grade", INTEGER_DATA_TYPE, false, null),
-                table.getColumns().get("grade"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("specialisation_id", INTEGER_DATA_TYPE, false,
-                        new SqlDatabaseStructure.ForeignKey(DATABASE_NAME, "specialisation", "id")),
-                table.getColumns().get("specialisation_id"));
-    }
-
-    @Test
-    @DisplayName("Test fit_wiki table")
-    void testFitWikiTable() {
-        SqlDatabaseStructure.Table table = databaseStructure.getSchemas().get(DATABASE_NAME).getTables().get("fit_wiki");
-        assertEquals(
-                new SqlDatabaseStructure.Column("identifier", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("identifier"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("data", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("data"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("author", INTEGER_DATA_TYPE, false,
-                        new SqlDatabaseStructure.ForeignKey(DATABASE_NAME, "user", "id")),
-                table.getColumns().get("author"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("reviewer_of_data", INTEGER_DATA_TYPE, false,
-                        new SqlDatabaseStructure.ForeignKey(DATABASE_NAME, "student", "id")),
-                table.getColumns().get("reviewer_of_data"));
-    }
-
-    @Test
-    @DisplayName("Test course table")
-    void testCourseTable() {
-        SqlDatabaseStructure.Table table = databaseStructure.getSchemas().get(DATABASE_NAME).getTables().get("course");
-        assertEquals(
-                new SqlDatabaseStructure.Column("(identifier of course", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("(identifier of course"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("name", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("name"));
-    }
-
-    @Test
-    @DisplayName("Test exam table")
-    void testExamTable() {
-        SqlDatabaseStructure.Table table = databaseStructure.getSchemas().get(DATABASE_NAME).getTables().get("exam");
-        assertEquals(
-                new SqlDatabaseStructure.Column("student", INTEGER_DATA_TYPE, true,
-                        new SqlDatabaseStructure.ForeignKey(DATABASE_NAME, "student", "id")),
-                table.getColumns().get("student"));
-        assertEquals(
-                new SqlDatabaseStructure.Column("course", INTEGER_DATA_TYPE, true, new SqlDatabaseStructure.ForeignKey(
-                        DATABASE_NAME,
-                        "course",
-                        "(identifier of course")),
-                table.getColumns().get("course"));
-    }
-
-
-    /**
-     * Verify that the table contains the only expected columns and primary keys.
-     *
-     * @param table       table to verify
-     * @param name        name of the table
-     * @param primaryKeys primary keys of the table
-     * @param columns     columns of the table
-     */
-    private void verifyTable(
-            SqlDatabaseStructure.Table table,
-            String name,
-            List<String> primaryKeys,
-            List<String> columns) {
-
-        assertEquals(name, table.getName());
-        assertEquals(columns.size(), table.getColumns().size());
-        assertEquals(primaryKeys.size(), table.getPrimaryKeys().size());
-        for (String column : columns) {
-            assertTrue(table.getColumns().containsKey(column));
-        }
-        for (String primaryKey : primaryKeys) {
-            assertTrue(table.getPrimaryKeys().contains(primaryKey));
-        }
     }
 }
--- backend/src/test/java/com/janbabak/noqlbackend/service/database/PostgresServiceTest.java
+++ backend/src/test/java/com/janbabak/noqlbackend/service/database/PostgresServiceTest.java
@@ -1,16 +1,9 @@
 package com.janbabak.noqlbackend.service.database;
 
-import com.janbabak.noqlbackend.dao.AbstractLocalDatabaseTest;
-import com.janbabak.noqlbackend.error.exception.DatabaseConnectionException;
-import com.janbabak.noqlbackend.error.exception.DatabaseExecutionException;
-import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure;
 import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure.Schema;
 import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure.Table;
-import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure.Column;
-import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure.ForeignKey;
 import com.janbabak.noqlbackend.model.entity.Database;
 import com.janbabak.noqlbackend.service.utils.FileUtils;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,30 +16,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ActiveProfiles("test")
 @SpringBootTest
-class PostgresServiceTest extends AbstractLocalDatabaseTest {
+class PostgresServiceTest extends AbstractSqlServiceTest {
 
-    @SuppressWarnings("FieldCanBeLocal")
-    private PostgresService postgresService;
+    PostgresServiceTest() {
+        super();
 
-    private SqlDatabaseStructure databaseStructure;
-    public Database getDatabase() {
-        return postgresDatabase;
+        INTEGER_DATA_TYPE = "integer";
+        NUMERIC_DATA_TYPE = "numeric";
+        VARCHAR_DATA_TYPE = "character varying";
+        TEXT_DATA_TYPE = "text";
+        CHAR_DATA_TYPE = "character";
+        BOOLEAN_DATA_TYPE = "boolean";
+        DATE_DATA_TYPE = "date";
+        TIMESTAMP_DATA_TYPE = "timestamp without time zone";
     }
-
-    private final String INTEGER_DATA_TYPE = "integer";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String NUMERIC_DATA_TYPE = "numeric";
-    private final String VARCHAR_DATA_TYPE = "character varying";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String TEXT_DATA_TYPE = "text";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String CHAR_DATA_TYPE = "character";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String BOOLEAN_DATA_TYPE = "boolean";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String DATE_DATA_TYPE = "date";
-    @SuppressWarnings("FieldCanBeLocal")
-    private final String TIMESTAMP_DATA_TYPE = "timestamp without time zone";
 
     /**
      * Get scripts for initialization of the databases
@@ -57,20 +40,70 @@ class PostgresServiceTest extends AbstractLocalDatabaseTest {
                 FileUtils.getFileContent("./src/test/resources/dbInsertScripts/postgres/allTables.sql"));
     }
 
-    @BeforeAll
+    protected Database getDatabase() {
+        return postgresDatabase;
+    }
+
+    protected SqlDatabaseService getSqlService(Database database) {
+        return new PostgresService(database);
+    }
+
+    protected String getDefaultSchema() {
+        return "public";
+    }
+
+    protected String getCvutSchema() {
+        return "cvut";
+    }
+
+    protected Table getUserTable() {
+        return getTable(getDefaultSchema(), "user");
+    }
+
+    protected Table getAddressTable() {
+        return getTable(getDefaultSchema(), "address");
+    }
+
+    protected Table getOrderTable() {
+        return getTable(getDefaultSchema(), "order");
+    }
+
+    protected Table getSpecialisationTable() {
+        return getTable(getCvutSchema(), "specialisation");
+    }
+
+    protected Table getStudentTable() {
+        return getTable(getCvutSchema(), "student");
+    }
+
+    protected Table getFitWikiTable() {
+        return getTable(getCvutSchema(), "fit_wiki");
+    }
+
+    protected Table getCourseTable() {
+        return getTable(getCvutSchema(), "course");
+    }
+
+    protected Table getExamTable() {
+        return getTable(getCvutSchema(), "exam");
+    }
+
     @Override
-    protected void setUp() throws DatabaseConnectionException, DatabaseExecutionException {
-        super.setUp();
-        postgresService = new PostgresService(getDatabase());
-        databaseStructure = postgresService.retrieveSchema();
+    protected String getUserTableName() {
+        return "\"user\"";
+    }
+
+    @Override
+    protected String getCourseIdentifier() {
+        return "\"(identifier of course\"";
     }
 
     @Test
     @DisplayName("Test schemas")
     void testSchemas() {
         assertEquals(2, databaseStructure.getSchemas().size());
-        assertTrue(databaseStructure.getSchemas().containsKey("public"));
-        assertTrue(databaseStructure.getSchemas().containsKey("cvut"));
+        assertTrue(databaseStructure.getSchemas().containsKey(getDefaultSchema()));
+        assertTrue(databaseStructure.getSchemas().containsKey(getCvutSchema()));
     }
 
     @Test
@@ -116,202 +149,4 @@ class PostgresServiceTest extends AbstractLocalDatabaseTest {
         verifyTable(schema.getTables().get("exam"), "exam", List.of("student", "course"),
                 List.of("student", "course"));
     }
-
-    @Test
-    @DisplayName("Test user table")
-    void testUserTable() {
-        Table table = databaseStructure.getSchemas().get("public").getTables().get("user");
-        assertEquals(
-                new Column("id", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("id"));
-        assertEquals(
-                new Column("name", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("name"));
-        assertEquals(
-                new Column("age", INTEGER_DATA_TYPE, false, null),
-                table.getColumns().get("age"));
-        assertEquals(
-                new Column("sex", CHAR_DATA_TYPE, false, null),
-                table.getColumns().get("sex"));
-        assertEquals(
-                new Column("email", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("email"));
-        assertEquals(
-                new Column("created_at", TIMESTAMP_DATA_TYPE, false, null),
-                table.getColumns().get("created_at"));
-    }
-
-    @Test
-    @DisplayName("Test address table")
-    void testAddressTable() {
-        Table table = databaseStructure.getSchemas().get("public").getTables().get("address");
-        assertEquals(
-                new Column("id", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("id"));
-        assertEquals(
-                new Column("user_id", INTEGER_DATA_TYPE, false,
-                        new ForeignKey("public", "\"user\"", "id")),
-                table.getColumns().get("user_id"));
-        assertEquals(
-                new Column("street", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("street"));
-        assertEquals(
-                new Column("city", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("city"));
-        assertEquals(
-                new Column("state", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("state"));
-        assertEquals(
-                new Column("postal_code", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("postal_code"));
-    }
-
-    @Test
-    @DisplayName("Test order table")
-    void testOrderTable() {
-        Table table = databaseStructure.getSchemas().get("public").getTables().get("order");
-        assertEquals(
-                new Column("id", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("id"));
-        assertEquals(
-                new Column("user_id", INTEGER_DATA_TYPE, false,
-                        new ForeignKey("public", "\"user\"", "id")),
-                table.getColumns().get("user_id"));
-        assertEquals(
-                new Column("order_date", DATE_DATA_TYPE, false, null),
-                table.getColumns().get("order_date"));
-        assertEquals(
-                new Column("total_amount", NUMERIC_DATA_TYPE, false, null),
-                table.getColumns().get("total_amount"));
-        assertEquals(
-                new Column("payment_method", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("payment_method"));
-        assertEquals(
-                new Column("shipping_address_id", INTEGER_DATA_TYPE, false,
-                        new ForeignKey("public", "address", "id")),
-                table.getColumns().get("shipping_address_id"));
-        assertEquals(
-                new Column("is_shipped", BOOLEAN_DATA_TYPE, false, null),
-                table.getColumns().get("is_shipped"));
-        assertEquals(
-                new Column("tracking_number", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("tracking_number"));
-        assertEquals(
-                new Column("status", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("status"));
-        assertEquals(
-                new Column("notes", TEXT_DATA_TYPE, false, null),
-                table.getColumns().get("notes"));
-    }
-
-    @Test
-    @DisplayName("Test specialisation table")
-    void testSpecialisationTable() {
-        Table table = databaseStructure.getSchemas().get("cvut").getTables().get("specialisation");
-        assertEquals(
-                new Column("id", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("id"));
-        assertEquals(
-                new Column("name", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("name"));
-        assertEquals(
-                new Column("manager", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("manager"));
-    }
-
-    @Test
-    @DisplayName("Test student table")
-    void testStudentTable() {
-        Table table = databaseStructure.getSchemas().get("cvut").getTables().get("student");
-        assertEquals(
-                new Column("id", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("id"));
-        assertEquals(
-                new Column("name", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("name"));
-        assertEquals(
-                new Column("birthdate", DATE_DATA_TYPE, false, null),
-                table.getColumns().get("birthdate"));
-        assertEquals(
-                new Column("grade", INTEGER_DATA_TYPE, false, null),
-                table.getColumns().get("grade"));
-        assertEquals(
-                new Column("specialisation_id", INTEGER_DATA_TYPE, false,
-                        new ForeignKey("cvut", "specialisation", "id")),
-                table.getColumns().get("specialisation_id"));
-    }
-
-    @Test
-    @DisplayName("Test fit_wiki table")
-    void testFitWikiTable() {
-        Table table = databaseStructure.getSchemas().get("cvut").getTables().get("fit_wiki");
-        assertEquals(
-                new Column("identifier", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("identifier"));
-        assertEquals(
-                new Column("data", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("data"));
-        assertEquals(
-                new Column("author", INTEGER_DATA_TYPE, false,
-                        new ForeignKey("public", "\"user\"", "id")),
-                table.getColumns().get("author"));
-        assertEquals(
-                new Column("reviewer_of_data", INTEGER_DATA_TYPE, false,
-                        new ForeignKey("cvut", "student", "id")),
-                table.getColumns().get("reviewer_of_data"));
-    }
-
-    @Test
-    @DisplayName("Test course table")
-    void testCourseTable() {
-        Table table = databaseStructure.getSchemas().get("cvut").getTables().get("course");
-        assertEquals(
-                new Column("(identifier of course", INTEGER_DATA_TYPE, true, null),
-                table.getColumns().get("(identifier of course"));
-        assertEquals(
-                new Column("name", VARCHAR_DATA_TYPE, false, null),
-                table.getColumns().get("name"));
-    }
-
-    @Test
-    @DisplayName("Test exam table")
-    void testExamTable() {
-        Table table = databaseStructure.getSchemas().get("cvut").getTables().get("exam");
-        assertEquals(
-                new Column("student", INTEGER_DATA_TYPE, true,
-                        new ForeignKey("cvut", "student", "id")),
-                table.getColumns().get("student"));
-        assertEquals(
-                new Column("course", INTEGER_DATA_TYPE, true, new ForeignKey(
-                        "cvut",
-                        "course",
-                        "\"(identifier of course\"")),
-                table.getColumns().get("course"));
-    }
-
-    /**
-     * Verify that the table contains the only expected columns and primary keys.
-     *
-     * @param table       table to verify
-     * @param name        name of the table
-     * @param primaryKeys primary keys of the table
-     * @param columns     columns of the table
-     */
-    private void verifyTable(
-            SqlDatabaseStructure.Table table,
-            String name,
-            List<String> primaryKeys,
-            List<String> columns) {
-
-        assertEquals(name, table.getName());
-        assertEquals(columns.size(), table.getColumns().size());
-        assertEquals(primaryKeys.size(), table.getPrimaryKeys().size());
-        for (String column : columns) {
-            assertTrue(table.getColumns().containsKey(column));
-        }
-        for (String primaryKey : primaryKeys) {
-            assertTrue(table.getPrimaryKeys().contains(primaryKey));
-        }
-    }
-
 }
