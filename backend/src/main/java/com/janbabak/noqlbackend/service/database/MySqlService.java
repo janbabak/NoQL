710538bf788938a95d49d41d100a6fc1445a11e3
Jan Babák <babakhonza@gmail.com>
2024-06-23 10:32:20 +0200

    Refactor Services

--- backend/src/main/java/com/janbabak/noqlbackend/service/database/MySqlService.java
+++ backend/src/main/java/com/janbabak/noqlbackend/service/database/MySqlService.java
@@ -14,70 +14,13 @@ import com.janbabak.noqlbackend.model.entity.Database;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class MySqlService extends BaseDatabaseService {
-
-    private static final String TABLE_SCHEMA_COLUMN_NAME = "table_schema";
-    private static final String TABLE_NAME_COLUMN_NAME = "table_name";
-    private static final String COLUMN_NAME_COLUMN_NAME = "column_name";
-    private static final String DATA_TYPE_COLUMN_NAME = "data_type";
-    private static final String PRIMARY_KEY_COLUMN_NAME = "primary_key";
+public class MySqlService extends SqlDatabaseService {
 
     public MySqlService(Database database) {
         databaseDAO = new MySqlDAO(database);
     }
 
-    /**
-     * Retrieves database schema (structure of tables, columns, etc...)
-     *
-     * @return database structure
-     * @throws DatabaseConnectionException cannot establish connection with the database
-     * @throws DatabaseExecutionException  query execution failed (syntax error)
-     */
-    @Override
-    public SqlDatabaseStructure retrieveSchema() throws DatabaseConnectionException, DatabaseExecutionException {
-
-        SqlDatabaseStructure dbStructure = new SqlDatabaseStructure();
-
-        retrieveSchemasTablesColumns(dbStructure);
-        retrieveForeignKeys(dbStructure);
-
-        return dbStructure;
-    }
-
-    // TODO: move to sql database service ancestor
-    /**
-     * Retrieves database information about schemas, tables and columns, primary keys, (omits relations)
-     *
-     * @param dbStructure empty database structure
-     * @throws DatabaseConnectionException cannot establish connection with the database
-     * @throws DatabaseExecutionException  query execution failed (syntax error)
-     */
-    private void retrieveSchemasTablesColumns(SqlDatabaseStructure dbStructure)
-            throws DatabaseConnectionException, DatabaseExecutionException {
-        try (ResultSetWrapper result = databaseDAO.getSchemasTablesColumns()) {
-            ResultSet resultSet = result.resultSet();
-            while (resultSet.next()) {
-                String tableSchema = resultSet.getString(TABLE_SCHEMA_COLUMN_NAME);
-                String tableName = resultSet.getString(TABLE_NAME_COLUMN_NAME);
-                String columnName = resultSet.getString(COLUMN_NAME_COLUMN_NAME);
-                String dataType = resultSet.getString(DATA_TYPE_COLUMN_NAME);
-                Boolean primaryKey = resultSet.getBoolean(PRIMARY_KEY_COLUMN_NAME);
-
-                SqlDatabaseStructure.Schema schema =
-                        dbStructure.getSchemas().computeIfAbsent(tableSchema, SqlDatabaseStructure.Schema::new);
-
-                SqlDatabaseStructure.Table table =
-                        schema.getTables().computeIfAbsent(tableName, SqlDatabaseStructure.Table::new);
-
-                // columnName key definitely doesn't exist - no need of checking it out
-                table.getColumns().put(columnName, new SqlDatabaseStructure.Column(columnName, dataType, primaryKey));
-            }
-        } catch (SQLException e) {
-            throw new DatabaseExecutionException(e.getMessage());
-        }
-    }
-
-    private void retrieveForeignKeys(SqlDatabaseStructure dbStructure)
+    protected void retrieveForeignKeys(SqlDatabaseStructure dbStructure)
             throws DatabaseConnectionException, DatabaseExecutionException {
 
         try (ResultSetWrapper result = databaseDAO.getForeignKeys()) {
--- backend/src/main/java/com/janbabak/noqlbackend/service/database/PostgresService.java
+++ backend/src/main/java/com/janbabak/noqlbackend/service/database/PostgresService.java
@@ -6,6 +6,10 @@ import com.janbabak.noqlbackend.error.exception.DatabaseConnectionException;
 import com.janbabak.noqlbackend.error.exception.DatabaseExecutionException;
 import com.janbabak.noqlbackend.model.entity.Database;
 import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure;
+import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure.Schema;
+import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure.Table;
+import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure.Column;
+import com.janbabak.noqlbackend.model.database.SqlDatabaseStructure.ForeignKey;
 import org.antlr.v4.runtime.misc.Pair;
 
 import java.sql.*;
@@ -13,64 +17,10 @@ import java.sql.*;
 /**
  * Retrieves Postgres database information.
  */
-public class PostgresService extends BaseDatabaseService {
-    private static final String TABLE_SCHEMA_COLUMN_NAME = "table_schema";
-    private static final String TABLE_NAME_COLUMN_NAME = "table_name";
-    private static final String COLUMN_NAME_COLUMN_NAME = "column_name";
-    private static final String DATA_TYPE_COLUMN_NAME = "data_type";
-    private static final String PRIMARY_KEY_COLUMN_NAME = "primary_key";
-    private static final String FOREIGN_KEY_DEFINITION_COLUMN_NAME = "constraint_definition";
+public class PostgresService extends SqlDatabaseService {
 
     public PostgresService(Database database) {
         databaseDAO = new PostgresDAO(database);
-    }
-
-    /**
-     * Retrieves information about database schema - schemas, tables, columns, primary and foreign keys, ...
-     *
-     * @return database information
-     * @throws DatabaseConnectionException cannot establish connection with the database
-     * @throws DatabaseExecutionException  query execution failed (syntax error)
-     */
-    public SqlDatabaseStructure retrieveSchema() throws DatabaseConnectionException, DatabaseExecutionException {
-        SqlDatabaseStructure db = new SqlDatabaseStructure();
-
-        retrieveSchemasTablesColumns(db);
-        retrieveForeignKeys(db);
-
-        return db;
-    }
-
-    /**
-     * Retrieves database information about schemas, tables and columns, primary keys, (omits relations)
-     *
-     * @param db empty database
-     * @throws DatabaseConnectionException cannot establish connection with the database
-     * @throws DatabaseExecutionException  query execution failed (syntax error)
-     */
-    private void retrieveSchemasTablesColumns(SqlDatabaseStructure db)
-            throws DatabaseConnectionException, DatabaseExecutionException {
-        try (ResultSetWrapper result = databaseDAO.getSchemasTablesColumns()) {
-            ResultSet resultSet = result.resultSet();
-            while (resultSet.next()) {
-                String tableSchema = resultSet.getString(TABLE_SCHEMA_COLUMN_NAME);
-                String tableName = resultSet.getString(TABLE_NAME_COLUMN_NAME);
-                String columnName = resultSet.getString(COLUMN_NAME_COLUMN_NAME);
-                String dataType = resultSet.getString(DATA_TYPE_COLUMN_NAME);
-                Boolean primaryKey = resultSet.getBoolean(PRIMARY_KEY_COLUMN_NAME);
-
-                SqlDatabaseStructure.Schema schema =
-                        db.getSchemas().computeIfAbsent(tableSchema, SqlDatabaseStructure.Schema::new);
-
-                SqlDatabaseStructure.Table table =
-                        schema.getTables().computeIfAbsent(tableName, SqlDatabaseStructure.Table::new);
-
-                // columnName key definitely doesn't exist - no need of checking it out
-                table.getColumns().put(columnName, new SqlDatabaseStructure.Column(columnName, dataType, primaryKey));
-            }
-        } catch (SQLException e) {
-            throw new DatabaseExecutionException(e.getMessage());
-        }
     }
 
     /**
@@ -80,22 +30,20 @@ public class PostgresService extends BaseDatabaseService {
      * @throws DatabaseConnectionException cannot establish connection with the database
      * @throws DatabaseExecutionException  query execution failed (syntax error)
      */
-    private void retrieveForeignKeys(SqlDatabaseStructure db)
+    protected void retrieveForeignKeys(SqlDatabaseStructure db)
             throws DatabaseConnectionException, DatabaseExecutionException {
 
         try (ResultSetWrapper result = databaseDAO.getForeignKeys()) {
             while (result.resultSet().next()) {
-                String constraintDefinition = result.resultSet().getString(FOREIGN_KEY_DEFINITION_COLUMN_NAME);
+                String constraintDefinition = result.resultSet().getString("constraint_definition");
                 String referencingSchemaAndTable = result.resultSet().getString(TABLE_NAME_COLUMN_NAME);
-
                 ForeignKeyData foreignKeyData = parseForeignKey(referencingSchemaAndTable, constraintDefinition);
 
-                // insert that foreign key
-                SqlDatabaseStructure.Schema schema = db.getSchemas().get(foreignKeyData.referencingSchema);
+                Schema schema = db.getSchemas().get(foreignKeyData.referencingSchema);
                 if (schema == null) {
                     continue;
                 }
-                SqlDatabaseStructure.Table table = schema.getTables().get(foreignKeyData.referencingTable);
+                Table table = schema.getTables().get(foreignKeyData.referencingTable);
                 if (table == null) {
                     // table not found, because table name is in double quotes
                     table = schema.getTables().get(stripQuotes(foreignKeyData.referencingTable));
@@ -103,11 +51,11 @@ public class PostgresService extends BaseDatabaseService {
                         continue;
                     }
                 }
-                SqlDatabaseStructure.Column column = table.getColumns().get(foreignKeyData.referencingColumn);
+                Column column = table.getColumns().get(foreignKeyData.referencingColumn);
                 if (column == null) {
                     continue;
                 }
-                column.setForeignKey(new SqlDatabaseStructure.ForeignKey(
+                column.setForeignKey(new ForeignKey(
                         foreignKeyData.referencedSchema,
                         foreignKeyData.referencedTable,
                         foreignKeyData.referencedColumn
