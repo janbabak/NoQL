318ad588ee779561f5e5482c74480aea234c18f8
Jan Babák <babakhonza@gmail.com>
2024-07-03 09:09:21 +0200

    export only what is used outside the logger file

--- frontend/NoQL/src/services/loging/logger.ts
+++ frontend/NoQL/src/services/loging/logger.ts
@@ -1,18 +1,18 @@
-export interface LogFunction {
+interface LogFunction {
   (message?: any, ...optionalParams: any[]): void
 }
 
-export interface Logger {
+interface Logger {
   info: LogFunction
   warn: LogFunction
   error: LogFunction
 }
 
-export type LogLevel = 'info' | 'warn' | 'error'
+type LogLevel = 'info' | 'warn' | 'error'
 
 const NO_OP: LogFunction = (_message?: any, ..._optionalParams: any[]): void => {}
 
-export class ConsoleLogger implements Logger {
+class ConsoleLogger implements Logger {
   readonly info: LogFunction
   readonly warn: LogFunction
   readonly error: LogFunction
@@ -42,8 +42,10 @@ export class ConsoleLogger implements Logger {
   }
 }
 
-export const log = new ConsoleLogger({
+const log = new ConsoleLogger({
   level: import.meta.env.MODE === 'development'
     ? 'info'
     : 'error'
 })
+
+export { log }
