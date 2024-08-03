2d65ed611234cbb78b1f7b477750fafb94be6de3
Jan Babák <babakhonza@gmail.com>
2024-08-03 17:42:56 +0200

    create local validation

--- frontend/NoQL/src/pages/dashboard/CreateDatabaseDialog.tsx
+++ frontend/NoQL/src/pages/dashboard/CreateDatabaseDialog.tsx
@@ -66,45 +66,66 @@ export function CreateDatabaseDialog({ open, onClose }: CreateDatabaseDialogProp
               margin="dense"
               id="name"
               label="Name"
+              placeholder="My local postgres"
               type="text"
               fullWidth
               variant="standard"
-              {...register('name', {
-                required: 'Name is required'
-              })}
               error={!!errors.name}
               helperText={errors.name?.message}
+              {...register('name', {
+                required: 'Name is required',
+                maxLength: { value: 32, message: 'Name maximum allowed length is 32' },
+              })}
             />
 
             <div style={{ display: 'flex', gap: '1rem' }}>
               <TextField
                 margin="dense"
                 id="host"
-                label="host"
+                label="Host"
+                placeholder="localhost"
                 type="text"
                 fullWidth
                 variant="standard"
-                {...register('host')}
+                error={!!errors.host}
+                helperText={errors.host?.message}
+                {...register('host', {
+                  required: 'Name is required',
+                  maxLength: { value: 253, message: 'Host maximum length is 253' },
+                })}
               />
 
               <TextField
                 margin="dense"
                 id="port"
                 label="port"
+                placeholder="5432"
                 type="number"
                 fullWidth
                 variant="standard"
-                {...register('port')}
+                error={!!errors.port}
+                helperText={errors.port?.message}
+                {...register('port', {
+                  required: 'Port is required',
+                  min: { value: 1, message: 'Port has to be greater than 0'},
+                  pattern: { value: /^[0-9]+$/, message: 'Port must be an integer' },
+                })}
               />
 
               <TextField
                 margin="dense"
                 id="database"
                 label="database"
+                placeholder='eshopUsers'
                 type="text"
                 fullWidth
                 variant="standard"
-                {...register('database')}
+                error={!!errors.database}
+                helperText={errors.database?.message}
+                {...register('database', {
+                  required: 'Database is required',
+                  maxLength: { value: 253, message: 'Database maximum length is 253' },
+                })}
               />
             </div>
 
@@ -116,7 +137,12 @@ export function CreateDatabaseDialog({ open, onClose }: CreateDatabaseDialogProp
                 type="text"
                 fullWidth
                 variant="standard"
-                {...register('username')}
+                error={!!errors.username}
+                helperText={errors.username?.message}
+                {...register('username', {
+                  required: 'Username is required',
+                  maxLength: { value: 128, message: 'Username maximum length is 128' }
+                })}
               />
 
               <TextField
@@ -126,7 +152,12 @@ export function CreateDatabaseDialog({ open, onClose }: CreateDatabaseDialogProp
                 type="password"
                 fullWidth
                 variant="standard"
-                {...register('password')}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+                {...register('password', {
+                  required: 'Password is required',
+                  minLength: { value: 128, message: 'Password maximum length is 128' },
+                })}
               />
 
               <Controller
