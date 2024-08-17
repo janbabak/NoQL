0de869fef0cb8b0c1f4ecf510e2b33033cd36733
Jan Babák <babakhonza@gmail.com>
2024-08-17 13:26:23 +0200

    create custom model entity

---
+++ backend/src/main/java/com/janbabak/noqlbackend/model/entity/CustomModel.java
@@ -0,0 +1,41 @@
+package com.janbabak.noqlbackend.model.entity;
+
+import com.janbabak.noqlbackend.validation.FirstValidationGroup;
+import com.janbabak.noqlbackend.validation.SecondValidationGroup;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
+/**
+ * Custom LLM that can be plugged via API.
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomModel {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NotBlank(groups = FirstValidationGroup.class)
+    @Length(min = 1, max = 32, groups = SecondValidationGroup.class)
+    private String name;
+
+    @NotBlank(groups = FirstValidationGroup.class)
+    private String host;
+
+    @NotNull(groups = FirstValidationGroup.class)
+    @Min(value = 1, groups = SecondValidationGroup.class)
+    private Integer port;
+}
