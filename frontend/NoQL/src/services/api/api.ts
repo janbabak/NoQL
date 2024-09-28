a75e28d99a2ff0a435c1b1aa56acbded4fac2d6d
Jan Babák <babakhonza@gmail.com>
2024-09-28 18:14:25 +0200

    remove unused headers from api

--- frontend/NoQL/src/services/api/api.ts
+++ frontend/NoQL/src/services/api/api.ts
@@ -1,11 +1,6 @@
 import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse } from 'axios'
 import { log } from '../loging/logger.ts'
 
-// TODO: properly setup
-const jwt = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJob256aWtAZ21haWwuY29tIiwiaWF0IjoxNzI1MTE3MjQ1LCJleHAiOjE3MjUyMDM2NDV9.4oCm9owj7de-IsYqU8KJrQVaG8WYqeeWx2jAsjPJ8wxhAltW1YkMAc9cs2R2Ckhzh7v3Vg8RhRDQor8WPW7luw'
-
-const jwtValue = 'Bearer ' + jwt
-
 /** query parameter */
 interface ApiParameter {
   name: string
@@ -58,9 +53,6 @@ class Api {
     const requestConfig: AxiosRequestConfig = {
       url: this.createUrl(path, parameters),
       method: 'GET',
-      // headers: {
-      //   'Authorization': jwtValue
-      // }
     }
 
     return this.axiosInstance.request(requestConfig)
@@ -77,17 +69,14 @@ class Api {
   post(path: string,
        data: string | number | boolean | object | null,
        parameters: ApiParameter[] = [],
-       headers: { [key: string]: string } = {},
-       authenticate: boolean  = true
+       headers: { [key: string]: string } = {}
   ): Promise<AxiosResponse> {
 
     const requestConfig: AxiosRequestConfig = {
       url: this.createUrl(path, parameters),
       method: 'POST',
       data: data,
-      // headers: authenticate
-      //   ? { ...headers, 'Authorization': jwtValue }
-      //   : headers
+      headers: headers
     }
 
     return this.axiosInstance.request(requestConfig)
@@ -107,9 +96,6 @@ class Api {
       url: this.createUrl(path, parameters),
       method: 'PUT',
       data: data,
-      // headers: {
-      //   'Authorization': jwtValue
-      // }
     }
 
     return this.axiosInstance.request(requestConfig)
@@ -124,9 +110,6 @@ class Api {
     const requestConfig: AxiosRequestConfig = {
       url: this.createUrl(path, parameters),
       method: 'DELETE',
-      // headers: {
-      //   'Authorization': jwtValue
-      // }
     }
 
     return this.axiosInstance.request(requestConfig)
