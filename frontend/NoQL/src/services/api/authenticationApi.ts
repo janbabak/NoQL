5d043d45ee137d2fed0bbfb162595c578c656a43
Jan Babák <babakhonza@gmail.com>
2024-10-02 18:37:06 +0200

    refactor authenticationApi.ts

--- frontend/NoQL/src/services/api/authenticationApi.ts
+++ frontend/NoQL/src/services/api/authenticationApi.ts
@@ -1,17 +1,25 @@
-import Api from './api.ts'
-import { AxiosResponse } from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { AuthenticationRequest, AuthenticationResponse, RegisterRequest } from '../../types/Authentication.ts'
+import { localStorageService } from '../LocalStorageService.ts'
 
 export const authenticationApi = {
-  API: Api.getInstance(),
   DOMAIN: '/auth',
+
+  axiosInstance: axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL,
+    timeout: parseInt(import.meta.env.VITE_API_TIMEOUT_MILIS)
+  }),
 
   /**
    * Authenticate existing user
    * @param request data
    */
   authenticate(request: AuthenticationRequest): Promise<AxiosResponse<AuthenticationResponse>> {
-    return this.API.post(this.DOMAIN + '/authenticate', request)
+    return this.axiosInstance.request({
+      url: this.DOMAIN + '/authenticate',
+      method: 'POST',
+      data: request
+    })
   },
 
   /**
@@ -19,6 +27,27 @@ export const authenticationApi = {
    * @param request data
    */
   register(request: RegisterRequest): Promise<AxiosResponse<AuthenticationResponse>> {
-    return this.API.post(this.DOMAIN + '/register', request)
-  }
+    return this.axiosInstance.request({
+      url: this.DOMAIN + '/register',
+      method: 'POST',
+      data: request
+    })
+  },
+
+  /**
+   * Obtain new access token using refresh token. If refresh token is not available, reject promise.
+   * @private
+   */
+  refreshToken(): Promise<AxiosResponse<AuthenticationResponse>> {
+    const refreshToken = localStorageService.getRefreshToken()
+    if (!refreshToken) {
+      return Promise.reject(new Error('No refresh token available'))
+    }
+
+    return this.axiosInstance.request({
+      url: this.DOMAIN + '/refreshToken',
+      method: 'POST',
+      data: refreshToken
+    })
+  },
 }
