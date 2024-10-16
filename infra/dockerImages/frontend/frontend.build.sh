30cecd6a9471e1d78f1e6e52c5e66413ca91d935
Jan Babák <babakhonza@gmail.com>
2024-10-16 14:59:09 +0200

    change versions

--- infra/dockerImages/frontend/frontend.build.sh
+++ infra/dockerImages/frontend/frontend.build.sh
@@ -2,7 +2,7 @@
 
 # build the frontend docker image
 
-VERSION=0.0.3
+VERSION=0.0.4
 IMAGE_NAME=janbabak/noql-frontend
 DOCKER_FILE=infra/dockerImages/frontend/frontend.Dockerfile
 
---
+++ infra/scripts/rebuild-fe-and-be.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+./infra/dockerImages/frontend/frontend.build.sh
+./infra/dockerImages/backend/backend.build.sh
