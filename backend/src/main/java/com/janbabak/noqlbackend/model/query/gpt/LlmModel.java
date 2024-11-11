fbd2d30e14fc6a5ed5b819b77daee8a3735241b5
Jan Babák <61386198+janbabak@users.noreply.github.com>
2024-11-11 09:21:39 +0100

    338 add claude llm support (#339)

--- backend/src/main/java/com/janbabak/noqlbackend/model/query/gpt/LlmModel.java
+++ backend/src/main/java/com/janbabak/noqlbackend/model/query/gpt/LlmModel.java
@@ -15,7 +15,9 @@ public enum LlmModel {
     LLAMA3_70B("llama3-70b", "LLAMA3 70B"),
     LLAMA3_13B_CHAT("llama-13b-chat", "LLAMA3 13B chat"),
     GEMINI_1DOT5_PRO("gemini-1.5-pro", "Gemini 1.5 pro"),
-    GEMINI_1DOT5_FLASH("gemini-1.5-flash", "Gemini 1.5 flash");
+    GEMINI_1DOT5_FLASH("gemini-1.5-flash", "Gemini 1.5 flash"),
+    CLAUDE_3_5_SONNET_20241022("claude-3-5-sonnet-20241022", "Claude 3.5 sonnet"),
+    CLAUDE_3_5_HAIKU_20241022("claude-3-5-haiku-20241022", "Claude 3.5 haiku");
 
     private final String model;
     private final String label;
---
+++ backend/src/test/java/com/janbabak/noqlbackend/service/api/ClaudeApiServiceTest.java
@@ -0,0 +1,41 @@
+package com.janbabak.noqlbackend.service.api;
+
+import com.janbabak.noqlbackend.model.query.QueryRequest;
+import org.apache.coyote.BadRequestException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClaudeApiServiceTest {
+
+    private final ClaudeApiService claudeApiService = new ClaudeApiService();
+
+    @ParameterizedTest
+    @MethodSource("validRequestDataProvider")
+    void testValidateSuccessfulRequest(QueryRequest request) {
+        assertDoesNotThrow(() -> claudeApiService.validateRequest(request));
+    }
+
+    static QueryRequest[] validRequestDataProvider() {
+        return new QueryRequest[]{
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", "claude-3-5-sonnet-20241022"),
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", "claude-3-5-haiku-20241022")
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("badRequestDataProvider")
+    void testValidateBadRequest(QueryRequest request) {
+        assertThrows(BadRequestException.class, () -> claudeApiService.validateRequest(request));
+    }
+
+    static QueryRequest[] badRequestDataProvider() {
+        return new QueryRequest[]{
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", "gpt-4o"),
+                new QueryRequest(UUID.randomUUID(), "Find the oldest user", "llama-13b-chat"),
+        };
+    }
+}
