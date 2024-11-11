412ddbf1539757e664b4ae7eab0cc767cc465749
Jan Babák <babakhonza@gmail.com>
2024-11-11 09:13:56 +0100

    add Claude support

---
+++ backend/src/main/java/com/janbabak/noqlbackend/service/api/ClaudeApiService.java
@@ -0,0 +1,103 @@
+package com.janbabak.noqlbackend.service.api;
+
+import com.janbabak.noqlbackend.error.exception.EntityNotFoundException;
+import com.janbabak.noqlbackend.error.exception.LLMException;
+import com.janbabak.noqlbackend.model.entity.ChatQueryWithResponse;
+import com.janbabak.noqlbackend.model.query.QueryRequest;
+import com.janbabak.noqlbackend.model.query.claude.ClaudeRequest;
+import com.janbabak.noqlbackend.model.query.claude.ClaudeResponse;
+import com.janbabak.noqlbackend.model.query.gemini.GeminiResponse;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@NoArgsConstructor
+public class ClaudeApiService implements QueryApi {
+
+    private final String CLAUDE_URL = "https://api.anthropic.com/v1/messages";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${app.credentials.anthropicApiKey}")
+    private String apiKey;
+
+    /**
+     * Send queries in chat form the model and retrieve a response.
+     *
+     * @param chatHistory  chat history
+     * @param queryRequest users query and model
+     * @param systemQuery  instructions from the NoQL system about task that needs to be done
+     * @param errors       list of errors from previous executions that should help the model fix its query
+     * @return model's response
+     * @throws LLMException        when LLM request fails.
+     * @throws BadRequestException when queryRequest is not valid
+     */
+    @Override
+    public String queryModel(
+            List<ChatQueryWithResponse> chatHistory,
+            QueryRequest queryRequest,
+            String systemQuery,
+            List<String> errors) throws LLMException, BadRequestException, EntityNotFoundException {
+
+        log.info("Chat with Claude API.");
+
+        validateRequest(queryRequest);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.add("x-api-key", apiKey);
+        headers.add("anthropic-version", "2023-06-01");
+
+        HttpEntity<ClaudeRequest> request = new HttpEntity<>(
+                new ClaudeRequest(chatHistory, queryRequest, systemQuery, errors), headers);
+
+        ResponseEntity<ClaudeResponse> responseEntity;
+
+        try {
+            responseEntity = restTemplate.postForEntity(CLAUDE_URL, request, ClaudeResponse.class);
+        } catch (RestClientException e) {
+            log.error("Claude API request failed: {}", e.getMessage());
+            throw new LLMException("Error while calling Claude API, try it later.");
+        }
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            return !Objects.requireNonNull(responseEntity.getBody()).content().isEmpty()
+                    ? responseEntity.getBody().content().get(0).text()
+                    : null;
+        }
+        if (responseEntity.getStatusCode().is4xxClientError()) {
+            log.error("Bad request to the Claude model, status_code={}, response={}.",
+                    responseEntity.getStatusCode(), responseEntity.getBody());
+            throw new LLMException("Bad request to the Claude model, we are working on it.");
+        }
+        if (responseEntity.getStatusCode().is5xxServerError()) {
+            log.error("Error on Claude side, status_code={}, response={}.",
+                    responseEntity.getStatusCode(), responseEntity.getBody());
+            throw new LLMException("Error on Gemini side, try it latter");
+        }
+        return null;
+    }
+
+    /**
+     * Validate request
+     *
+     * @param queryRequest users request
+     * @throws BadRequestException unsupported model
+     */
+    void validateRequest(QueryRequest queryRequest) throws BadRequestException {
+        if (queryRequest.getModel() == null || !queryRequest.getModel().startsWith("claude")) {
+            log.error("Model is missing in the request.");
+            throw new BadRequestException("Model is missing in the request.");
+        }
+    }
+}
--- backend/src/main/java/com/janbabak/noqlbackend/service/api/LlmApiServiceFactory.java
+++ backend/src/main/java/com/janbabak/noqlbackend/service/api/LlmApiServiceFactory.java
@@ -10,6 +10,7 @@ public class LlmApiServiceFactory {
     private final GptApiService gptApiService;
     private final LlamaApiService llamaApiService;
     private final GeminiApiService geminiApiService;
+    private final ClaudeApiService claudeApiService;
     private final CustomModelApiService customModelApiService;
 
     /**
@@ -18,12 +19,12 @@ public class LlmApiServiceFactory {
      * @param model large language model to be used for the translation
      * @return correct query API service
      */
-    @SuppressWarnings("all") // default branch is unnecessary
     public QueryApi getQueryApiService(String model) {
         return switch (model) {
             case "gpt-4o", "gpt-4", "gpt-4-turbo", "gpt-4-32k", "gpt-3.5-turbo" -> gptApiService;
             case "llama3-70b", "llama-13b-chat" -> llamaApiService;
             case "gemini-1.5-pro", "gemini-1.5-flash" -> geminiApiService;
+            case "claude-3-5-sonnet-20241022", "claude-3-5-haiku-20241022" -> claudeApiService;
             default -> customModelApiService;
         };
     }
