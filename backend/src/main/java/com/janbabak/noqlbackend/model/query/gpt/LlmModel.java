4da5da84e6f55d6251b5d5cd17b6718773c71d66
Jan Babák <babakhonza@gmail.com>
2024-12-18 08:32:30 +0100

    remove obsolete and bad models

--- backend/src/main/java/com/janbabak/noqlbackend/model/query/gpt/LlmModel.java
+++ backend/src/main/java/com/janbabak/noqlbackend/model/query/gpt/LlmModel.java
@@ -9,17 +9,12 @@ import lombok.Getter;
 public enum LlmModel {
     GPT_4o("gpt-4o", "GPT 4o"),
     GPT_4o_MINI("gpt-4o-mini", "GPT 4o mini"),
-    GPT_4("gpt-4", "GPT 4"),
     GPT_4_TURBO("gpt-4-turbo", "GPT 4 turbo"),
-    GPT_4_32K("gpt-4-32k", "GPT 4 32k"),
     GPT_3_5_TURBO("gpt-3.5-turbo", "GPT 3.5 turbo"),
-    LLAMA3_1_405B("llama3.1-405b", "LLAMA 3.1 405B"),
-    LLAMA3_70B("llama3.1-70b", "LLAMA 3.1 70B"),
-    LLAMA3_13B_CHAT("llama-13b-chat", "LLAMA3 13B chat"),
     GEMINI_1DOT5_PRO("gemini-1.5-pro", "Gemini 1.5 pro"),
     GEMINI_1DOT5_FLASH("gemini-1.5-flash", "Gemini 1.5 flash"),
-    CLAUDE_3_5_SONNET_20241022("claude-3-5-sonnet-20241022", "Claude 3.5 sonnet"),
-    CLAUDE_3_5_HAIKU_20241022("claude-3-5-haiku-20241022", "Claude 3.5 haiku");
+    CLAUDE_3_5_HAIKU_20241022("claude-3-5-haiku-20241022", "Claude 3.5 haiku"),
+    LLAMA3DOT1_70B("llama3.1-70b", "LLAMA 3.1 70B");
 
     private final String model;
     private final String label;
