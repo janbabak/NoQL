1323cfa341731586c1120739a406ba11ac083808
Jan Babák <babakhonza@gmail.com>
2025-12-28 20:04:46 +0100

    test

--- backend/src/main/java/com/janbabak/noqlbackend/NoQLBackendApplication.java
+++ backend/src/main/java/com/janbabak/noqlbackend/NoQLBackendApplication.java
@@ -1,4 +1,4 @@
-package com.janbabak.noqlbackend;
+package com.janbabak.noqlbackend; // test
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
