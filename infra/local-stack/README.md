b4ea961ee07c0e7548f20e5c41dc6b7d9cdb7f15
Jan Babák <61386198+janbabak@users.noreply.github.com>
2025-12-31 15:15:49 +0100

    466 update local stack docker compose (#467)
    Co-authored-by: github-actions <github-actions@github.com>

--- infra/local-stack/README.md
+++ infra/local-stack/README.md
@@ -1,62 +1,39 @@
-# Local Stack
+# Dev stacks
 
-- Run backend locally with all dependencies on a single click.
-- Can be used for frontend development.
-- The script starts backend in a docker container with its dependencies (database, plot service) also as separate containers, and it starts example database and insert sample data.
-- Run all commands from the NoQL root directory!
+## Backend Dev Stack
 
-## Local variables
-It is necessary to create a `NoQL/backend/.env.local` file with the following content:
-```dotenv
-# external services/apis
-OPEN_AI_API_KEY="" # input your api key
-GEMINI_API_KEY="" # input your api key
-CLAUDE_API_KEY="" # input your api key
+- **Docker compose stack containing:**
+  - Backend server
+  - Plot service
+  - Backend database
+  - Example Postgres database
 
-NOQL_DB_NAME="database"
-NOQL_DB_HOST="localhost"
-NOQL_DB_PORT="5432"
-NOQL_DB_USERNAME="user"
-NOQL_DB_PASSWORD="password"
+## Database Dev Stack
 
-# Local databases - should match the NOQL_DB_xxx credentials
-POSTGRES_PASSWORD="password"
-POSTGRES_USER="user"
-POSTGRES_DB="database"
+- **Docker compose stack containing:**
+    - Plot service
+    - Backend database
+    - Example Postgres database
+    - Example MySQL database
+  
+## Usage
 
-# settings
-PAGINATION_MAX_PAGE_SIZE=100
-PAGINATION_DEFAULT_PAGE_SIZE=20
+- **Create stack:**
+    ```bash
+    ./infra/local-stack/scripts/backend-stack.sh create <databse|backend>
+    ```
 
-PLOT_SERVICE_CONTAINER_NAME="plot-service-dev-stack"
-DEFAULT_USER_QUERY_LIMIT=150
+- **Start existing stack:**
+    ```bash
+    ./infra/local-stack/scripts/backend-stack.sh start <databse|backend> 
+    ```
 
-# security
-JWT_SECRET="fkajlak4jt34ktj34t98vu44d5d4p54o5d45m34ik5n345m34wm5l431145l434u64bgjsuicvkaplcvqyevasswilmvbti09478jujhhdsbfasdhfbu4" # could be changed
-# 30 min
-JWT_EXPIRATION=1800
-# 7 days
-JWT_REFRESH_EXPIRATION=604800
-```
+- **Stop stack:**
+    ```bash
+    ./infra/local-stack/scripts/backend-stack.sh stop <databse|backend> 
+    ```
 
-## Create and start local stack
-```bash
-chmod +x ./infra/local-stack/scripts/create-local-stack.sh
-./infra/local-stack/scripts/create-local-stack.sh
-```
-
-## Start local stack
-```bash
-docker compose -f infra/local-stack/local-stack.docker-compose.yaml up -d
-```
-
-## Stop local stack
-```bash
-docker compose -f infra/local-stack/local-stack.docker-compose.yaml stop
-```
-
-## Build new version of local stack
-```bash
-chmod +x infra/local-stack/rebuild-stack.sh 
-./infra/local-stack/scripts/rebuild-stack.sh
-```
+- **Remove stack** (keep data):
+    ```bash
+    ./infra/local-stack/scripts/backend-stack.sh remove <databse|backend>
+    ```
---
+++ infra/local-stack/scripts/stack.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# This script manages backend-stack (stack containing backend,  plot service, database, and example database)
+
+STACK_NAME="" # changed based on input
+BACKEND_URL="http://localhost:8080"
+BACKEND_STACK_DOCKER_COMPOSE_FILE="infra/local-stack/backend-stack.docker-compose.yaml"
+DATABASE_STACK_DOCKER_COMPOSE_FILE="infra/local-stack/databases-stack.docker-compose.yaml"
+DOCKER_COMPOSE_FILE=""
+STACK_TYPE=""
+
+EXAMPLE_DB_SQL_INIT_SCRIPT="./sample-data.sql"
+EXAMPLE_DB_CONTAINER=""
+EXAMPLE_DB_NAME="database"
+EXAMPLE_DB_USER="user"
+EXAMPLE_DB_PASSWORD="password"
+EXAMPLE_DB_PORT=5432
+
+USER_EMAIL="user@email.com"
+USER_PASSWORD="12345678"
+USER_ID=""
+AUTH_TOKEN=""
+
+createStack() {
+    echo "Creating a local stack"
+    runDockerCompose
+    insertSampleData
+
+    if [[ "$STACK_TYPE" == "backend" ]]; then
+        registerNewUser
+        authenticateUser
+        registerNewDatabase
+    fi
+}
+
+startLocalStack() {
+    echo "Starting local stack"
+    docker compose --file $DOCKER_COMPOSE_FILE --project-name ${STACK_NAME} start
+}
+
+stopLocalStack() {
+    echo "Stopping local stack"
+    docker compose --file ${DOCKER_COMPOSE_FILE} --project-name ${STACK_NAME} stop
+}
+
+removeLocalStack() {
+    echo "Removing local stack"
+    docker compose --file ${DOCKER_COMPOSE_FILE} --project-name ${STACK_NAME} down
+}
+
+usage() {
+    echo "Usage: $0 {create|start|stop|remove} {backend|database}"
+    echo
+    echo "Commands:"
+    echo "  create    Create the local stack, insert sample data, register user and database"
+    echo "  start     Start existing containers"
+    echo "  stop      Stop running containers"
+    echo "  remove    Stop and remove containers and network"
+    echo "  database  Use database stack"
+    echo "  backend   Use backend stack"
+}
+
+runDockerCompose() {
+    docker compose \
+        --file ${DOCKER_COMPOSE_FILE} \
+        --env-file  backend/.env.local \
+        --project-name ${STACK_NAME} \
+        up --detach
+
+    sleep 20
+    echo "Local stack started"
+}
+
+insertSampleData() {
+    echo "Inserting sample data into the example database..."
+
+    docker exec -i "$EXAMPLE_DB_CONTAINER" \
+    psql -U "$EXAMPLE_DB_USER" -d "$EXAMPLE_DB_NAME" -f "$EXAMPLE_DB_SQL_INIT_SCRIPT"
+
+    sleep 20
+    echo "Init script executed"
+}
+
+registerNewUser() {
+    RESPONSE=$(curl --location --silent --show-error --write-out "HTTPSTATUS:%{http_code}" \
+        --request POST "${BACKEND_URL}/auth/register" \
+        --header "Content-Type: application/json" \
+        --data-raw "{
+            \"email\": \"${USER_EMAIL}\",
+            \"password\": \"${USER_PASSWORD}\",
+            \"firstName\": \"user\",
+            \"lastName\": \"user\"
+        }")
+
+    # Separate body and status code
+    HTTP_BODY=$(echo "$RESPONSE" | sed -e 's/HTTPSTATUS\:.*//g')
+    HTTP_STATUS=$(echo "$RESPONSE" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+    if [ "$HTTP_STATUS" -eq 201 ]; then
+        echo "User registered successfully."
+    else
+        echo "Failed to register user. Server response:"
+        echo "$HTTP_BODY"
+        echo "HTTP code: $HTTP_STATUS"
+        exit 1
+    fi
+}
+
+authenticateUser() {
+    RESPONSE=$(curl --location --silent --show-error --write-out "HTTPSTATUS:%{http_code}" \
+        --request POST "${BACKEND_URL}/auth/authenticate" \
+        --header "Content-Type: application/json" \
+        --data-raw "{
+            \"email\": \"${USER_EMAIL}\",
+            \"password\": \"${USER_PASSWORD}\"
+        }")
+
+    # Separate body and status code
+    HTTP_BODY=$(echo "$RESPONSE" | sed -e 's/HTTPSTATUS\:.*//g')
+    HTTP_STATUS=$(echo "$RESPONSE" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+    if [ "$HTTP_STATUS" -eq 200 ]; then
+        # Extract accessToken and userId using jq
+        AUTH_TOKEN=$(echo "$HTTP_BODY" | jq -r '.accessToken')
+        USER_ID=$(echo "$HTTP_BODY" | jq -r '.user.id')
+        echo "Authentication successful."
+        echo "UserId: $USER_ID"
+        echo "Access token: $AUTH_TOKEN"
+    else
+        echo "Authentication failed. Server response:"
+        echo "$HTTP_BODY"
+        echo "HTTP code: $HTTP_STATUS"
+        exit 2
+    fi
+}
+
+registerNewDatabase() {
+    RESPONSE=$(curl --location --silent --show-error --write-out "HTTPSTATUS:%{http_code}" \
+        --request POST "${BACKEND_URL}/database" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: Bearer ${AUTH_TOKEN}" \
+        --data-raw "{
+            \"name\" : \"Example Postgres\",
+            \"host\" : \"postgres\",
+            \"port\" : ${EXAMPLE_DB_PORT},
+            \"database\" : \"${EXAMPLE_DB_NAME}\",
+            \"userName\" : \"${EXAMPLE_DB_USER}\",
+            \"password\" : \"${EXAMPLE_DB_PASSWORD}\",
+            \"engine\" : \"POSTGRES\",
+            \"userId\" : \"${USER_ID}\",
+            \"createDefaultChat\" : true
+        }")
+
+    # Separate body and status code
+    HTTP_BODY=$(echo "$RESPONSE" | sed -e 's/HTTPSTATUS\:.*//g')
+    HTTP_STATUS=$(echo "$RESPONSE" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+    if [ "$HTTP_STATUS" -eq 201 ]; then
+        echo "Database registered successfully."
+    else
+        echo "Failed to register database. Server response:"
+        echo "$HTTP_BODY"
+        echo "HTTP code: $HTTP_STATUS"
+        exit 3
+    fi
+}
+
+if [ $# -le 1 ]; then
+    usage
+    exit 1
+fi
+
+case "$2" in
+    database)
+        DOCKER_COMPOSE_FILE=$DATABASE_STACK_DOCKER_COMPOSE_FILE
+        EXAMPLE_DB_CONTAINER="example-postgres-database-stack"
+        STACK_TYPE=$2
+        STACK_NAME="${2}-dev-stack"
+    ;;
+    backend)
+        DOCKER_COMPOSE_FILE=$BACKEND_STACK_DOCKER_COMPOSE_FILE
+        EXAMPLE_DB_CONTAINER="example-postgres-backend-stack"
+        STACK_TYPE=backend
+        STACK_NAME="${2}-dev-stack"
+    ;;
+    *)
+        usage
+        exit 1
+esac
+
+
+case "$1" in
+  create)
+    createStack
+    ;;
+  start)
+    startLocalStack
+    ;;
+  stop)
+    stopLocalStack
+    ;;
+  remove)
+    removeLocalStack
+    ;;
+  *)
+    usage
+    exit 2
+    ;;
+esac
